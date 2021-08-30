5dd210c5a6c0f2b65615347208cb08009cbb9a49
saurabhshcs <saurabhshcs@gmail.com>
2021-08-30 02:16:05 +0100

    1

---
+++ graphql-query/src/main/java/com/techsharezone/graphql/repository/SubjectRepository.java
@@ -0,0 +1,13 @@
+package com.techsharezone.graphql.repository;
+
+/*
+ * @created 30/08/2021 -02:13
+ * @project graphql
+ * @author  saurabhshcs
+ */
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SubjectRepository {
+}
